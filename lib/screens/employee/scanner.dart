a91b8f5e8dce5cd4f323e9861377c1140921260d
TrushaT <trusha.talati@spit.ac.in>
2020-12-19 17:26:45 +0530

    scan Log created

--- lib/screens/employee/scanner.dart
+++ lib/screens/employee/scanner.dart
@@ -42,14 +42,186 @@ class _ScannerState extends State<Scanner> {
             child: Center(
               child: Text('Scan Text: $qrText'),
             ),
-            flex: 1)
+            flex: 1),
+        RaisedButton(onPressed: () {
+          if (qrText != null) {
+             users.document(qrText.toString()).get().then((DocumentSnapshot doc) {
+      if (doc.exists) {
+        FirebaseFirestore.instance.collection("ScanLog").add({
+          "scandate": Timestamp.fromDate(DateTime.now()),
+          "employee_id": user.uid,
+          "product_id": doc.id,
+        });
+        setState(() {
+          showProduct = true;
+        });
+        print(doc.data());
+        setState(() {
+          data = doc.data();
+        });
+      }
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return showProduct
+            ? Scaffold(
+                appBar:
+                    AppBar(title: Text('Employee Pannel'), actions: <Widget>[]),
+                body: SingleChildScrollView(
+                  child: Container(
+                      child: Column(
+                    children: [
+                      Container(
+                        child: Center(
+                          child: Text(
+                            "Product Name",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(30),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text(
+                          "Product Description  :",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(10),
+                        margin: EdgeInsets.fromLTRB(5, 15, 0, 0),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(data["description"],
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify),
+                        ),
+                        padding: const EdgeInsets.all(25),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text("Product Status  :  " + data["status"],
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text("Product Cost  :  " + data["cost"],
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text(
+                            "Product Added Date  : " +
+                                data["dateAdded"].toDate().toString(),
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text(
+                            "Waranty  : " +
+                                data["warranty"].toDate().toString(),
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text(
+                            "End of Life  : " +
+                                data["endOfLife"].toDate().toString(),
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text(
+                            "Next Service Date  : " +
+                                data["nextServiceDate"].toDate().toString(),
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text("Product Type  : " + data["productType"],
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        child: Text("Department  : " + data["department"],
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify),
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.all(20),
+                        margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        decoration: myBoxDecoration(),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 20, 0, 40),
+                        child: MaterialButton(
+                          height: 50.0,
+                          minWidth: 3 * MediaQuery.of(context).size.width / 4,
+                          onPressed: () => {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) {
+                              print(qrText.toString());
+                              return MyCustomForm(qrText.toString());
+                            }))
+                          },
+                          color: Colors.white,
+                          child: Container(
+                            child: Text("Add Service"),
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.green)),
+                        ),
+                      )
+                    ],
+                  )),
+                ))
+            : Scaffold(
+                appBar:
+                    AppBar(title: Text('Employee Pannel'), actions: <Widget>[]),
+                body: Text('Error, Product not found!'),
+              );
+      }));
+    });
+          } else {
+            null;
+          }
+        },
+        child: Text('See Product')
+)
       ],
     ));
   }
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
@@ -58,171 +230,6 @@ class _ScannerState extends State<Scanner> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         qrText = scanData;
-      });
-      users.document(qrText.toString()).get().then((DocumentSnapshot doc) {
-        if (doc.exists) {
-          FirebaseFirestore.instance.collection("ScanLog").add({
-            "scandate": Timestamp.fromDate(DateTime.now()),
-            "employee_id": user.uid,
-            "product_id": doc.id,
-          });
-          setState(() {
-            showProduct = true;
-          });
-          print(doc.data());
-          setState(() {
-            data = doc.data();
-          });
-        }
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (BuildContext context) {
-          return showProduct
-              ? Scaffold(
-                  appBar: AppBar(
-                      title: Text('Employee Pannel'), actions: <Widget>[]),
-                  body: SingleChildScrollView(
-                    child: Container(
-                        child: Column(
-                      children: [
-                        Container(
-                          child: Center(
-                            child: Text(
-                              "Product Name",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 30),
-                            ),
-                          ),
-                          padding: const EdgeInsets.all(30),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text(
-                            "Product Description  :",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(10),
-                          margin: EdgeInsets.fromLTRB(5, 15, 0, 0),
-                        ),
-                        Container(
-                          child: Center(
-                            child: Text(data["description"],
-                                style: TextStyle(fontSize: 16),
-                                textAlign: TextAlign.justify),
-                          ),
-                          padding: const EdgeInsets.all(25),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text("Product Status  :  " + data["status"],
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text("Product Cost  :  " + data["cost"],
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text(
-                              "Product Added Date  : " +
-                                  data["dateAdded"].toDate().toString(),
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text(
-                              "Waranty  : " +
-                                  data["warranty"].toDate().toString(),
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text(
-                              "End of Life  : " +
-                                  data["endOfLife"].toDate().toString(),
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text(
-                              "Next Service Date  : " +
-                                  data["nextServiceDate"].toDate().toString(),
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text("Product Type  : " + data["productType"],
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          child: Text("Department  : " + data["department"],
-                              style: TextStyle(fontSize: 20),
-                              textAlign: TextAlign.justify),
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(20),
-                          margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                          decoration: myBoxDecoration(),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 20, 0, 40),
-                          child: MaterialButton(
-                            height: 50.0,
-                            minWidth: 3 * MediaQuery.of(context).size.width / 4,
-                            onPressed: () => {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (BuildContext context) {
-                                print(qrText.toString());
-                                return MyCustomForm(qrText.toString());
-                              }))
-                            },
-                            color: Colors.white,
-                            child: Container(
-                              child: Text("Add Service"),
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.green)),
-                          ),
-                        )
-                      ],
-                    )),
-                  ))
-              : Scaffold(
-                  appBar: AppBar(
-                      title: Text('Employee Pannel'), actions: <Widget>[]),
-                  body: Text('Error, Product not found!'),
-                );
-        }));
       });
     });
   }
