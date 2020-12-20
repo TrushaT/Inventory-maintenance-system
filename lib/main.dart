3f666affdee82c0627666f07a4f1ccdca1737a58
prachimehta203 <pnmehta203@gmail.com>
2020-12-20 15:46:25 +0530

    logo and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:inventory_management/screens/authenticate/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,13 +14,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.white,
+      home: AnimatedSplashScreen(
+        splash: Image.asset(
+          'assets/logo.png',
+          height: 500,
+          width: 500,
+          scale: 0.8,
+        ),
+        nextScreen: Login(),
+        splashTransition: SplashTransition.scaleTransition,
+        backgroundColor: Colors.lightBlue[300],
+        duration: 2000,
       ),
-      home: Login(),
     );
   }
 }
+
+// title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//         scaffoldBackgroundColor: Colors.white,
+//       ),
+//       home: Login(),
